57ef7e3f2ccfd46c089c2a61da987250d72ed8b0
W. Matthew Wilson <matt@216software.com>
2014-12-22 16:06:28 -0500

    Added dependency to setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ from setuptools import setup
 
 setup(
     name="LogToPG",
-    version="0.0.1",
+    version="0.0.2",
     packages=["logtopg"],
     author="216 Software, LLC",
     author_email="info@216software.com",
     license="BSD License",
     include_package_data=True,
+
+    install_requires=[
+        'psycopg2',
+    ]
 )
