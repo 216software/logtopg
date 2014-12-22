bfc272d1adca1f4ddf3d4a042428746d7f21bcc6
W. Matthew Wilson <matt@216software.com>
2014-12-22 18:15:07 -0500

    Hopefully got package data to be included

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="LogToPG",
-    version="0.0.2",
+    version="0.0.3",
     packages=["logtopg"],
     author="216 Software, LLC",
     author_email="info@216software.com",
