2ba6d3a960b57643fd209bcc44d409fa08bc0a2f
W. Matthew Wilson <matt@216software.com>
2014-12-23 12:11:22 -0500

    Maybe fixed the python2.6 zero-length field name in format

--- logtopg/createtable.sql
+++ logtopg/createtable.sql
@@ -1,4 +1,4 @@
-create table if not exists {} (
+create table if not exists {0} (
     created text,
     name ltree,
     loglevel int,
--- logtopg/insertrow.sql
+++ logtopg/insertrow.sql
@@ -1,4 +1,4 @@
-insert into {} (
+insert into {0} (
     created,
     name,
     loglevel,
